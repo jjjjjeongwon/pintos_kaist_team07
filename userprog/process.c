7d72a894fe5b0d547e5b51f3049ae3c04a76e370
Hyungjun <dlgudwns1207@gmail.com>
2023-06-22 13:29:58 +0900

    feat: stack_grow temp work

--- userprog/process.c
+++ userprog/process.c
@@ -812,7 +812,6 @@ static bool
 setup_stack (struct intr_frame *if_) {
 	bool success = false;
 	void *stack_bottom = (void *) (((uint8_t *) USER_STACK) - PGSIZE);
-
 	/* TODO: Map the stack on stack_bottom and claim the page immediately.
 	 * TODO: If success, set the rsp accordingly.
 	 * TODO: You should mark the page is stack. */
@@ -820,12 +819,12 @@ setup_stack (struct intr_frame *if_) {
 	// NOTE: 당신은 vm/vm.h의 vm_type에 있는 
 	// 보조 marker(예 - VM_MARKER_0)들을 페이지를 마킹하는데 사용할 수 있습니다.
 	vm_alloc_page(VM_ANON, stack_bottom, true);
+	struct page *stack_page = spt_find_page(&thread_current()->spt, stack_bottom);
+	stack_page->uninit.type |= VM_MARKER_0;
 	success = vm_claim_page(stack_bottom);
+
 	if (success)
 		if_->rsp = USER_STACK;
-	else {
-		
-	}
 	return success;
 }
 #endif /* VM */
